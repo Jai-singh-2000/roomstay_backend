0b2bf74a8ade441c38f92a968de749fe5864c351
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-02-13 16:27:11 +0530

    add: Hotel model added

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,43 +1,37 @@
 const mongosse = require("mongoose");
 
-const userSchema = mongosse.Schema(
-    {
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        firstName: {
-            type: String,
-            required: true
-        },
-        lastName: {
-            type: String
-        },
-        phoneNo: {
-            type: String
-        },
-        isVerified: {
-            type: Boolean,
-            required: true,
-            default: false
-        },
-        isAdmin: {
-            type: Boolean,
-            required: true,
-            default: false
-        }
+const userSchema = mongosse.Schema({
+    email: {
+        type: String,
+        required: true
     },
-
-    {
-        timestamps: true
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String
+    },
+    phoneNo: {
+        type: String
+    },
+    isVerified: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
     }
-)
+}, { timestamps: true })
 
-const User = mongosse.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
 
