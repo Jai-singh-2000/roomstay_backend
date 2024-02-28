76da3fe566e6ea3dd50cef1be881d5286716ce84
satender <sksatenderkumar59@gmail.com>
2024-02-28 16:27:31 +0530

    room: room api create

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = 3000
 const userRoutes = require("./routes/UserRouter")
 const hotelRoutes = require("./routes/HotelRouter")
 const floorRoutes = require("./routes/FloorRouter")
+const roomRoutes = require("./routes/RoomRouter")
 const connectDatabase = require("./config/connectDb");
 const authToken = require("./middlewares/tokenMiddleware")
 const mailConnection=require("./config/mail")
@@ -16,6 +17,7 @@ mailConnection({mail:"jai.singh.corporate@gmail.com",subject:"Roomstay",text:"Ab
 app.use(express.json())
 app.use(userRoutes)
 app.use(hotelRoutes)
+app.use(roomRoutes)
 app.use(authToken, floorRoutes)
 
 app.listen(PORT, () => {
