ea2cb32f4fcb102947f2b00f51456180ddefce99
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-12 17:18:56 +0530

    add: payment razorpay init api

---
+++ routes/PaymentRouter.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const route = express.Router();
+const PaymentController = require("../controller/PaymentController");
+
+route.post("/paymentInit", PaymentController.paymentInit);
+
+module.exports = route;
