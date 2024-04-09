da8242c34f190e28e9dac0b183166be0eca2b8d6
suraj <surajgautam56878@gmail.com>
2024-04-09 16:33:01 +0530

    add:payment backend code change

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,39 +1,39 @@
-const AdminOrder = require("../models/AdminOrderModel");
-const Payment = require("../models/PaymentModel");
-const Plan = require("../models/PlanModel");
-const User = require("../models/UserModel");
+const AdminOrder = require("../models/AdminOrderModel")
+const Payment = require("../models/PaymentModel")
+const Plan = require("../models/PlanModel")
+const User = require("../models/UserModel")
 
 async function getAdminOrder(req, res) {
   try {
-    const { orderId } = req.params;
-    const adminOrder = await AdminOrder.findOne({ _id: orderId });
+    const { orderId } = req.params
+    const adminOrder = await AdminOrder.findOne({ _id: orderId })
     if (adminOrder) {
       res.status(200).json({
         success: true,
         order: adminOrder,
-      });
-      return;
+      })
+      return
     }
     res.status(404).json({
       success: false,
       message: "Something went wrong",
-    });
+    })
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    console.log(err)
+    res.status(500).json(err)
   }
 }
 
 async function createAdminOrder(req, res) {
   try {
-    const userId = req.userId;
+    const userId = req.userId
     const {
       plan,
       razorpayId,
       razorpayPaymentId,
       razorpaySignature,
       paymentId,
-    } = req.body;
+    } = req.body
 
     if (
       !plan ||
@@ -45,28 +45,28 @@ async function createAdminOrder(req, res) {
       res.status(400).json({
         success: false,
         message: "Invalid request",
-      });
-      return;
+      })
+      return
     }
 
     // Is that plan exist or not
-    const planDocument = await Plan.findOne({ _id: plan });
+    const planDocument = await Plan.findOne({ _id: plan })
     if (!planDocument) {
       res.status(404).json({
         success: false,
         message: "Plan Not Found",
-      });
-      return;
+      })
+      return
     }
 
     // Is that same plan exist in user document
-    const existingUser = await User.findOne({ _id: userId });
+    const existingUser = await User.findOne({ _id: userId })
     if (existingUser.plan === planDocument?.name) {
       res.status(404).json({
         success: false,
         message: "Plan Already Exist",
-      });
-      return;
+      })
+      return
     }
 
     // Add paymentId and signature in payment document
@@ -75,51 +75,55 @@ async function createAdminOrder(req, res) {
         razorpayId: razorpayId,
       },
       { razorpayPaymentId, razorpaySignature }
-    );
+    )
 
     if (!existingPayment) {
       res.status(404).json({
         success: false,
         message: "Payment Not Found",
-      });
-      return;
+      })
+      return
     }
 
     const userDoc = await User.findOneAndUpdate(
       { _id: userId },
       { plan: planDocument?.name }
-    );
+    )
 
     if (!userDoc) {
       res.status(404).json({
         success: false,
         message: "Plan Not Updated",
-      });
-      return;
+      })
+      return
     }
 
     const newOrder = {
       User: userId,
       Payment: paymentId,
       plan: plan,
-    };
+    }
 
-    const adminOrder = await AdminOrder.create(newOrder);
+    const adminOrder = await AdminOrder.create(newOrder)
     if (adminOrder) {
       res.status(200).json({
         success: true,
         message: "Purchased successfully",
-      });
-      return;
+      })
+      return
     }
     res.status(404).json({
       success: false,
       message: "Something went wrong",
-    });
+    })
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    console.log(err)
+    res.status(500).json(err)
   }
 }
 
-module.exports = { getAdminOrder, createAdminOrder };
+async function createRoomOrder(req, res) {
+  const userId = req.userId
+}
+
+module.exports = { getAdminOrder, createAdminOrder }
